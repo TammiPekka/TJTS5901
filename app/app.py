d0dc247ce89540379955d60631f613e418a95529
kmjaaske <kaisajaaskelainen@outlook.com>
2025-02-24 13:13:47 +0200

    yhdistetty weather api toimimaan myos openweatherille

--- app/app.py
+++ app/app.py
@@ -15,7 +15,9 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 @app.route("/")
 def home():
     nimi = "Jukka"
-    return render_template("home.html", nimi=nimi)
+    temperature_weather = "10"
+    temperature_open = "20"
+    return render_template("home.html", nimi=nimi, temperature_weather=temperature_weather)
 
 @app.route("/home")
 def health():
@@ -26,7 +28,7 @@ def tester():
     return "testing endpoint, aasd,asdf,asd, test"
 
 
-@app.route('/get_temperature', methods=['GET', 'POST'])
+"""@app.route('/get_temperature', methods=['GET', 'POST'])
 def get_temperature():
     city = request.form.get("city") if request.method == "POST" else request.args.get("city")
 
@@ -46,7 +48,8 @@ def get_temperature():
         return jsonify({"temperature1": data["main"]["temp"]})  #Return temperature of the city in json format
     else:
         return jsonify({"error": data.get("message", "Unknown error")}), response.status_code
+        """
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
--- app/test_weather_api.py
+++ app/test_weather_api.py
@@ -6,22 +6,39 @@ from dotenv import load_dotenv
 load_dotenv()
 
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+OPEN_W_API_KEY = os.getenv("OPEN_W_API_KEY")
+
+url_weather = "http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+url_open = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
 # An example of a function that makes an API call
-def get_weather_data(city):
-    api_key = WEATHER_API_KEY  # korvaa tämä WeatherAPI:n API-avaimella
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+def get_weather_data(api_key, url_template, city):
+    url = url_template.format(api_key=api_key, city=city)
     response = requests.get(url)
     
     if response.status_code != 200:
         raise Exception("API call failed")
     
-    return response.json()
+    data = response.json()
+
+    if "current" in data:  # WeatherAPI
+        return data["current"]["temp_c"]
+    elif "main" in data:  # OpenWeather
+        return data["main"]["temp"] - 273.15  # Convert from Kelvin to Celsius
+    else:
+        raise Exception("Invalid response format")
+
+    
 
 # Test for a function
 def test_get_weather_data():
     city = "London"
-    data = get_weather_data(city)
-    return data
+    weather_data = get_weather_data(WEATHER_API_KEY, url_weather, city)
+    openweather_data = get_weather_data(OPEN_W_API_KEY, url_open, city)
+    return weather_data, openweather_data
 
 if __name__ == "__main__":
-    print(test_get_weather_data())
+    weather_data, openweather_data = test_get_weather_data()
+    print("WeatherAPI data:", weather_data)
+    print("OpenWeather data:", openweather_data)
+    
