f858fcde47b677973e330afef4d97b430cd72033
kmjaaske <kaisajaaskelainen@outlook.com>
2025-03-05 20:52:29 +0200

    added some exceptions

--- app/api_openw.py
+++ app/api_openw.py
@@ -14,9 +14,16 @@ def get_open_data(city):
     url_open = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     response = requests.get(url_open)
     
+    #Catch errors
+    if response.status_code == 404:
+        raise ValueError("City not found")
+    
+    if response.status_code == 401:
+        raise KeyError("API key is invalid")
+    
     if response.status_code != 200:
         raise Exception("API call failed")
-    
+
     return response.json()
 
 # Test for a function
