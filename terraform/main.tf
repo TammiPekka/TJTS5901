c4ba5c4554f266fecfa686e6367c1cf48a24bb71
ptjtammi <pekka.tj.tammi@student.jyu.fi>
2025-03-15 14:17:14 +0200

    Updated main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,3 +58,73 @@ resource "google_monitoring_alert_policy" "high_cpu_alert" {
     content = "High CPU usage detected on Flask container in GKE."
   }
 }
+
+#Create alert for high memory usage
+resource "google_monitoring_alert_policy" "high_memory_alert" {
+  display_name = "GKE Flask App High Memory Alert"
+
+  conditions {
+    display_name = "High Memory Usage"
+    condition_threshold {
+      filter          = "metric.type=\"kubernetes.io/container/memory/used_bytes\" AND resource.type=\"k8s_container\""
+      comparison      = "COMPARISON_GT"
+      threshold_value = 0.8  # Alert if memory usage > 80%
+      duration        = "60s"
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_SUM"
+      }
+    }
+  }
+
+  combiner             = "OR"
+  notification_channels = [google_monitoring_notification_channel.email1.id,
+                           google_monitoring_notification_channel.email2.id,
+                           google_monitoring_notification_channel.email3.id]
+
+  documentation {
+    content = "High memory usage detected on Flask container in GKE."
+  }
+}
+
+# Create an Alert for High Disk Usage
+resource "google_monitoring_alert_policy" "high_node_disk_alert" {
+  display_name = "GKE Flask App High Disk Usage Alert"
+
+  conditions {
+    display_name = "High Disk Usage"
+    condition_threshold {
+      filter          = "metric.type=\"compute.googleapis.com/instance/disk/read_bytes_count\" AND resource.type=\"gce_instance\""
+      comparison      = "COMPARISON_GT"
+      threshold_value = 10000000  
+      duration        = "60s"
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
+    }
+  }
+
+  combiner             = "OR"
+  notification_channels = [google_monitoring_notification_channel.email1.id,
+                           google_monitoring_notification_channel.email2.id,
+                           google_monitoring_notification_channel.email3.id]
+
+  documentation {
+    content = "High disk usage detected on Flask container in GKE."
+  }
+}
+
+resource "google_compute_network" "flask_network" {
+  name = "flask-network"
+}
+
+resource "google_compute_firewall" "flask_allow_https" {
+  name    = "flask-allow-https"
+  network = google_compute_network.flask_network.id
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
